fdfdb80d12bd4966b819627add2c8517ae0d3863
Feitoza, Diego <diego.feitoza@yahoo.com.br>
2019-10-01 14:57:40 -0300

    Finalizando API Rest

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    return res.json({message: "Hello World!"})
+//GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.body = Acessar corpo da requisição (para criação e edição)
+//req.params = Acessar route params (para edição e delete)
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-4eb0q.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
