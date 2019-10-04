03faaa6e259a7551baaa6b4ea41b72fc7b2d4b76
Feitoza, Diego <diego.feitoza@yahoo.com.br>
2019-10-03 23:08:49 -0300

    Iniciando desenvolvimento mobile com React Native

---
+++ aulas/mobile/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './src/routes';
+
+//View == <div> box de container
+//Text == <qualquer tag de texto> Qualquer campo de texto, e tudo tem que ser estilizado individualmente
+//StyleSheet == 
+//Para aplicar css no Native, é preciso criar um style dentro da tag que vai usar a estilização
+//O react Native utiliza flex-box *importante
+
+
+export default function App() {
+  return (<Routes />);
+}
